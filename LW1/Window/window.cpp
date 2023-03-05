6107c655ae6a2d59393c31c2d971fda97ea8eb88
ana-STAS-iia <kalach.03@bk.ru>
2023-03-05 22:05:43 +0500

    Window
    Window

---
+++ LW1/Window/window.cpp
@@ -0,0 +1,31 @@
+﻿#include <GL/freeglut.h>
+
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT); //очищает буферы до предустановленных значений
+    glutSwapBuffers();
+}
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB); //обратный вызов, который отрисовывает 1 кадр
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv); //инициализирую GLUT
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(500, 500); //размер окна
+    glutInitWindowPosition(100, 100); //расположение окна
+    glutCreateWindow("Tutorial 01"); //имя окна
+
+    InitializeGlutCallbacks();
+
+    glClearColor(0.0f, 0.5f, 0.0f, 0.0f); //устанавливаю цвет, который будет использоваться при очистке буфера кадра
+
+    glutMainLoop();
+
+    return 0;
+}
